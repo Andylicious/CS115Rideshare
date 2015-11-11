79f1ccffa96339594991729f7f848dc6128fbaef
Andrew Lien <andrew.lien@thecodingblog.org>
2015-11-10 21:52:12 -0800

    useless commit, lol

--- SCIonic/www/js/controllers.js
+++ SCIonic/www/js/controllers.js
@@ -201,15 +201,26 @@ angular.module('starter.controllers', [])
                     $scope.comments_block = [];
 
 
-                    for(var i = 0; i < Report.length; i++){
-                      var report_side = Report[i].innerText.trim();
-                      var date_comment = date_info[i].innerText.trim();
-                      var report_comments = Class[i+1].innerText.trim();
-                      var report_type = Type[i].innerText.trim();
-                      var course = report_comments.split(/[ ,]+/);
-                      var course_name = course[0];
-                      var course_credit = course[1] + " " + course[2] + " " + course[3];
-                      var course_attendence = course[4] + " " + course[5]; 
+  
+                    $scope.quality_score = "";
+                    $scope.average_grade = "";
+                    $scope.easiness_score = "";
+                    $scope.clarity_score = "";
+                    $scope.helpfulness_score = "";
+
+                    //console.log(Quality);
+                    if(found == true){
+                      $scope.quality_score = Quality[0].innerText;
+                      $scope.average_grade = Quality[1].innerText;
+                      for(var i = 0; i < Report.length; i++){
+                        var report_side = Report[i].innerText.trim();
+                        var date_comment = date_info[i].innerText.trim();
+                        var report_comments = Class[i+1].innerText.trim();
+                        var report_type = Type[i].innerText.trim();
+                        var course = report_comments.split(/[ ,]+/);
+                        var course_name = course[0];
+                        var course_credit = course[1] + " " + course[2] + " " + course[3];
+                        var course_attendence = course[4] + " " + course[5]; 
 
                       
                       $scope.comments_block[i] = { side: report_side, 
@@ -218,19 +229,17 @@ angular.module('starter.controllers', [])
                                                   comments: {
                                                               name: course_name,
                                                               credit: course_credit,
-                                                              attendence: course_attendence }}
+                                                              attendence: course_attendence 
                                                             }
+                                                 }
+                      }
                    
                     $scope.easiness_score = easiness[1];
                     $scope.clarity_score = clarity[1];
                     $scope.helpfulness_score = helpfulness[1];
-                    $scope.quality_score = "";
-                    $scope.average_grade = "";
 
-                    //console.log(Quality);
-                    if(found == true){
-                      $scope.quality_score = Quality[0].innerText;
-                      $scope.average_grade = Quality[1].innerText;
+
+
                       $ionicLoading.hide();
                     }else{
                       $scope.quality_score = "Score not found on RMP. N/A"
