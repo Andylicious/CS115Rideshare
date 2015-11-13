18be4f8ce93da5597cb7819c718348bf7ff080b5
Andrew Lien <andrew.lien@thecodingblog.org>
2015-11-12 23:25:03 -0800

    Made bookmarks a little bit more robust, added a textbooks pane

--- SCIonic/www/js/sharedLinks_service.js
+++ SCIonic/www/js/sharedLinks_service.js
@@ -4,6 +4,7 @@
  */
 angular.module('starter.controllers').service('sharedLinks',[function(){
   var course_link = "http://crossorigin.me/https://pisa.ucsc.edu/class_search/";
+  var books_link = '';
   return {
     get_course_link: function() {
             return course_link;
@@ -11,7 +12,16 @@ angular.module('starter.controllers').service('sharedLinks',[function(){
     set_course_link: function(value){
             course_link += value
            
+    },get_books_link: function() {
+            return books_link;
+        },
+    set_books_link: function(value){
+    	
+    		var filtered_value = value.substr(1,value.indexOf(',')-2);
+            books_link += filtered_value
+           
     }
+
 
       }
 
